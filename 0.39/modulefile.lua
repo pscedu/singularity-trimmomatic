fd77866f22962e92679fbe2ba84dd3079f47f747
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2021-04-23 16:15:06 -0400

    Added basic scripts

---
+++ 0.39/modulefile.lua
@@ -0,0 +1,47 @@
+--
+-- trimmomatic 0.39 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Biological Sciences"
+-- "Description: Trimmomatic performs a variety of useful trimming tasks for illumina paired-end and single ended data."
+
+whatis("Name: Trimmomatic")
+whatis("Version: 0.39")
+whatis("Category: Biological Sciences")
+whatis("URL: https://www.psc.edu/resources/software")
+whatis("Description: Trimmomatic performs a variety of useful trimming tasks for illumina paired-end and single ended data.")
+
+help([[
+trimmomatic 0.39
+----------------
+
+Description
+-----------
+Trimmomatic performs a variety of useful trimming tasks for illumina paired-end and single ended data. The selection of trimming steps and their associated parameters are supplied on the command line.
+
+To load the module type
+
+> module load trimmomatic/0.39
+
+To unload the module type
+
+> module unload trimmomatic/0.39
+
+Documentation
+-------------
+For help, type
+
+> trimmomatic --help
+
+Repository
+----------
+
+Tools included in this module are
+
+* trimmomatic
+]])
+
+local package = "trimmomatic"
+local version = "4.1.0"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
